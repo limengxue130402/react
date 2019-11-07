9c4b78068cbf68eb284a9f83d2028f52cb26dd62
limengxue130402 <2503114011@qq.com>
2019-11-07 17:59:26 +0800

    message

---
+++ build/precache-manifest.8453747118c6257bdc02f4c31168c7ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74cb20485b6b851594d179fe3eb3dab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db6143a8dcaf539a1af5",
+    "url": "/static/css/main.b67a7649.chunk.css"
+  },
+  {
+    "revision": "9749984f0ed4b1738c31",
+    "url": "/static/js/2.efc69861.chunk.js"
+  },
+  {
+    "revision": "db6143a8dcaf539a1af5",
+    "url": "/static/js/main.5ce03087.chunk.js"
+  },
+  {
+    "revision": "2c2f5b40e0aa5bc0163d",
+    "url": "/static/js/runtime-main.ab0defdc.js"
+  }
+]);
